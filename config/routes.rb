4a52d6ab90e52827d2f62ae5d808ca26c1f592fe
David Adams <david.riding.rails@gmail.com>
2019-02-27 14:43:54 -0700

    Mod routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # need a root
+  get  '/signup',  to: 'users#new'  
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :user, only: [:new, :create, :show]
 end
