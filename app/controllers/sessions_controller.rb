d75a873dfab5131749c05b2cef72bf0e6b164ab6
David Adams <david.riding.rails@gmail.com>
2019-02-27 15:35:54 -0700

    Update sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
+    @user = User.find_by(name: params[:session][:name])
+    if @user
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{@user.name}, you are now logged in"
       redirect_to root_path
     else
-      flash.now[:danger] = "Try again with valid credentials"
+      flash.now[:danger] = "Try again with another name"
       render :new
     end
   end
 
   def destroy
-    log_out if logged_in?
+    session.delete(:user_id)
+    @current_user = nil 
     redirect_to root_url
   end
 end
