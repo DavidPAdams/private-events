e2c698b1f233131c7442050a3a78452e57068957
David Adams <david.riding.rails@gmail.com>
2019-03-01 16:20:49 -0700

    Add welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # need a root
+  root 'welcome#home'
   get  '/signup',  to: 'users#new'  
   get  '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
