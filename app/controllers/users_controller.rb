32708c4ce78c2dba6a096b6bdf4099755b2cd321
David Adams <david.riding.rails@gmail.com>
2019-03-01 16:22:18 -0700

    Mod users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @my_events = Events.where("creator_id = ?" , params[:id])
+    @upcoming_events = current_user.upcoming_events
+    @prev_events = current_user.previous_events
   end
 
   private
