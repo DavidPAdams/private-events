7d836370d7fd1433d2bcb0265407824fc3cebe5e
David Adams <david.riding.rails@gmail.com>
2019-03-02 12:43:17 -0700

    Scope Event with upcoming & past

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendees, through: :invitations
   has_many :invitations, foreign_key: "attended_event_id"
+
+  scope :upcoming, -> { where("date >= ?", Time.zone.now).order("date ASC") }
+  scope :past, -> { where("dae < ?", Time.zone.now).order("date DESC") }
 end
