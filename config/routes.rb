0bc87eb9bf83394e4b17d194b957c00b05d371a7
David Adams <david.riding.rails@gmail.com>
2019-03-02 13:38:46 -0700

    Fix logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'  
   get  '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:index, :new, :create, :show]
 end
